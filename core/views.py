c1925e33cd0b70929406828f0ee7062f85e28285
NicoShanstrom <148294962+NicoShanstrom@users.noreply.github.com>
2025-11-25 16:24:57 -0500

    Creates urls in core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Employee
+from .serializers import EmployeeSerializer
 
-# Create your views here.
+class EmployeeViewSet(viewsets.ReadOnlyModelViewSet):
+  """
+  Read-only API for employees + their enrollments. This is basically a DRF version of serializers from Rails land
+  """
+  queryset = Employee.objects.all().order_by("employee_id")
+  serializer_class = EmployeeSerializer
